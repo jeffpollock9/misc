16185162e398091e2bc3e2ad6170a8eb4d853f90
jeffpollock9 <jeffrey.pollock@smartodds.co.uk>
2018-06-02 11:19:47 +0100

    try some openmp stuff

--- cuda/add_cpu_openmp.cpp
+++ cuda/add_cpu_openmp.cpp
@@ -2,7 +2,7 @@
 
 void add_cpu_openmp(const int n, const float* x, const float* y, float* z)
 {
-    #pragma omp parallel for
+    #pragma omp parallel for schedule(static) num_threads(12)
     for (int i = 0; i < n; ++i)
     {
         z[i] = x[i] + y[i];
